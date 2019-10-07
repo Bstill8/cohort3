f8e7418bfb9ba34ef50f47ba364c645efb0152cb
Brendan Stillwell <stillwellbrendan@gmail.com>
2019-10-07 13:59:00 -0600

    added hello world project

---
+++ src/Hello World/basic.js
@@ -0,0 +1,28 @@
+console.log("Hello World from basic.js")
+let currentheader = "Hello World from HTML"
+function size(int){
+    if(int>=20){
+        return "large";
+    }
+    if(int>=10 && int<20){
+        return "med";
+    }
+    return "small";
+}
+function clickevent() {
+    console.log("I'm in the click button event")
+    var x = 1 + parseInt(document.getElementById(12).value);
+    console.log(x);
+    var int = parseInt(document.getElementById(12).value);
+    document.getElementById(11).innerHTML = size(int);
+}
+function headerchange(name){
+    if(name == "Hello World from HTML"){
+        document.getElementById('header').innerHTML = "Hello from Canada";
+        currentheader = "Hello from Canada";
+    }
+    if(name == "Hello from Canada"){
+        document.getElementById('header').innerHTML = "Hello World from HTML";
+        currentheader = "Hello World from HTML";
+    }
+}

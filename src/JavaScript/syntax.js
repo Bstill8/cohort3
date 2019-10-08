fee0c2855349411cb975044d04fa8ffc6688a054
Brendan Stillwell <stillwellbrendan@gmail.com>
2019-10-08 16:38:59 -0600

    october 8 update

--- src/JavaScript/syntax.js
+++ src/JavaScript/syntax.js
@@ -1,3 +1,5 @@
+import { isTSExpressionWithTypeArguments } from "@babel/types";
+
 // define attributes / variables
 //  number
 //  string
@@ -7,10 +9,45 @@
 //  undefined
 const functions = {
     number:(num) => {
-        return null;
-    } 
+        return num + 3 + num*6;
+    } ,
+    string:(string) => {
+        return string + "ish";
+    },
+    boolean:(boolean) => {
+        if(boolean == true){
+            return false;
+        }
+        if(boolean == false){
+            return true;
+        }
+    },
+    array:(...args) => {
+        var big = new Array(6);
+        for(var i = 1; i <= args.length; i++){
+            big[i-1] = args[i-1];
+        }
+        return big;
 
-
+    },
+    objects:(number, string, array) => {
+        var cool = {
+            number: number,
+            string: string,
+            array: array
+        };
+        return cool;
+    },
+    undefined:() => {
+        var x;
+        return x;
+    },
+    arraymanip:(arr) => {
+        arr.push(4);
+        arr.unshift(1);
+        arr[1] = 5;
+        return arr;
+    }
 }
 
 export default functions;
--- src/JavaScript/syntax.test.js
+++ src/JavaScript/syntax.test.js
@@ -1,6 +1,12 @@
 import functions from './syntax';
-import { exportAllDeclaration } from '@babel/types';
 
-test('check number', () => {
-    expect(functions.number(5)).toBe(null)
+test('test functions', () => {
+    expect(functions.number(5)).toBe(38);
+    expect(functions.string("fool")).toBe("foolish");
+    expect(functions.boolean(true)).toBe(false);
+    expect(functions.array(1,2,3,4,5,6)).toEqual([1,2,3,4,5,6]);
+    expect(functions.objects(5,"fool",[1,2,3])).toEqual({number:5, string:"fool", array:[1,2,3]});
+    expect(functions.undefined()).toBe(undefined);
+    expect(functions.arraymanip([2,3])).toEqual([1,5,3,4]);
+    expect(functions.loops(1,1,1,1,1))
 })

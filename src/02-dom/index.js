6b8e14a4ed75bd3d3d2dda04856379fc8af2c3db
Brendan Stillwell <stillwellbrendan@gmail.com>
2019-10-16 13:15:02 -0600

    The basic DOM finished

--- src/02-dom/index.js
+++ src/02-dom/index.js
@@ -1,4 +1,19 @@
 document.getElementById("main").addEventListener("click", idfunction);
+document.getElementById("Show").addEventListener("click", show);
+document.getElementById("Add").addEventListener("click", add);
 function idfunction(event){
     console.log(event.target.id)
+}
+function show(event){
+    for(var i = 0; i<document.getElementById("list").children.length; i++){
+        console.log(document.getElementById("list").children[i].innerHTML)
+    }
+}
+var itemNumber = 4;
+function add(event){
+    var newItem = document.createElement("li");
+    var text = document.createTextNode("Item " + itemNumber.toString());
+    newItem.appendChild(text);
+    document.getElementById("list").appendChild(newItem);
+    itemNumber ++;
 }

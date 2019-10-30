b823ee2f31b1976988a1e6b45eef0d9b0f9226ee
Brendan Stillwell <stillwellbrendan@gmail.com>
2019-10-30 09:56:19 -0600

    retrieved work from Jason

--- src/03-Objects/functions/account.js
+++ src/03-Objects/functions/account.js
@@ -1,15 +1,63 @@
-export class account{
-    constructor(accountName, initialBalance){
-        this.name = accountName;
-        this.ammount = initialBalance;
-    }
-    deposit(value){
-        this.ammount += value;
-    }
-    withdraw(value){
-        this.ammount -= value;
-    }
-    balance(){
-        return this.ammount;
-    }
+export class account {
+  constructor(accountName, initialBalance) {
+    this.name = accountName;
+    this.amount = initialBalance;
+  }
+  deposit(value) {
+    this.amount += value;
+  }
+  withdraw(value) {
+    this.amount -= value;
+  }
+  balance() {
+    return `Your new balance is $${this.amount}`;
+  }
 }
+
+export class accountController {
+  constructor(){
+  this.allAccounts = [];
+  }
+
+  createAccount(name, balance){
+    let newInstance = new account(name, balance);
+    this.allAccounts.push(newInstance);
+  }
+
+  removeAccount(name){
+    for (let index = 0; index < this.allAccounts.length; index++) {
+      if (this.allAccounts[index].name == name) {
+        this.allAccounts.splice(index, 1);
+      }
+    }
+  }
+
+  sumAccounts(){
+      let sum = 0;
+      for (let index = 0; index < this.allAccounts.length; index++) {
+          sum += this.allAccounts[index].amount;
+          
+      }
+      return sum;
+  }
+
+  highAccount(){
+    let highest = 0;
+    for (let index = 0; index < this.allAccounts.length; index++) {
+        if(this.allAccounts[index].amount > highest) {
+            highest = this.allAccounts[index].amount;
+        }  
+    }
+    return highest;
+  }
+
+  lowAccount(){
+    let lowest = this.allAccounts[0].amount;
+    for (let index = 0; index < this.allAccounts.length; index++) {
+        if(this.allAccounts[index].amount < lowest) {
+            lowest = this.allAccounts[index].amount;
+        }  
+    }
+    return lowest;
+  }
+};
--- src/03-Objects/functions/account.test.js
+++ src/03-Objects/functions/account.test.js
@@ -1,29 +1,80 @@
-import {account} from './account.js'
+import {account, functions} from './account.js'
+
 test('create empty account', () => {
     var newInstance = new account('first', 25);
     var secondInstance = new account('second', 30);
-    expect(newInstance.ammount).toEqual(25);
-    expect(secondInstance.ammount).toEqual(30);
+    expect(newInstance.amount).toEqual(25);
+    expect(secondInstance.amount).toEqual(30);
     expect(newInstance.name).toEqual('first');
     expect(secondInstance.name).toEqual('second');
 });
 test('test deposit method', () => {
     var newInstance = new account('first', 25);
     newInstance.deposit(10);
-    expect(newInstance.ammount).toEqual(35);
+    expect(newInstance.amount).toEqual(35);
     newInstance.deposit(5);
-    expect(newInstance.ammount).toEqual(40);
+    expect(newInstance.amount).toEqual(40);
 });
 test('test withdraw method', () => {
     var newInstance = new account('first', 25);
     newInstance.withdraw(10);
-    expect(newInstance.ammount).toEqual(15);
+    expect(newInstance.amount).toEqual(15);
     newInstance.withdraw(15);
-    expect(newInstance.ammount).toEqual(0);
+    expect(newInstance.amount).toEqual(0);
 })
 test('test balance method', () => {
     var newInstance = new account('first', 25);
-    expect(newInstance.balance()).toEqual(25);
+    expect(newInstance.balance()).toEqual("Your new balance is $25");
     var secondInstance = new account('second', 30);
-    expect(secondInstance.balance()).toEqual(30);
+    expect(secondInstance.balance()).toEqual("Your new balance is $30");
+})
+
+const accountNames = () => {
+    return functions.allAccounts.map(acc => acc.name)    
+}
+
+test('test create account function', () => {
+    functions.allAccounts = [];
+    functions.createAccount("Savings", 25)
+    expect(accountNames()).toEqual(["Savings"]);
+    functions.createAccount("Car fund", 30)
+    expect(accountNames()).toEqual(["Savings", "Car fund"]);
+})
+
+test('test delete account function', () => {
+    functions.allAccounts = [];
+    functions.createAccount("Savings", 25)
+    functions.createAccount("Car fund", 30)
+    functions.removeAccount("Savings");
+    expect(accountNames()).toEqual(["Car fund"])
+    functions.createAccount("Holiday", 50)
+    functions.removeAccount("Car fund");
+    expect(accountNames()).toEqual(["Holiday"])
+})
+
+test('test sum accounts function', () => {
+    functions.allAccounts = [];
+    functions.createAccount("Savings", 25)
+    functions.createAccount("Car fund", 30)
+    expect(functions.sumAccounts()).toEqual(55)
+    functions.createAccount("Holiday", 50)
+    expect(functions.sumAccounts()).toEqual(105)
+})
+
+test('test highest accounts function', () => {
+    functions.allAccounts = [];
+    functions.createAccount("Savings", 25);
+    functions.createAccount("Car fund", 100);
+    expect(functions.highAccount()).toEqual(100);
+    functions.createAccount("Holiday", 50);
+    expect(functions.highAccount()).toEqual(100);
+})
+
+test('test lowest accounts function', () => {
+    functions.allAccounts = [];
+    functions.createAccount("Savings", 25);
+    functions.createAccount("Car fund", 100);
+    expect(functions.lowAccount()).toEqual(25);
+    functions.createAccount("Holiday", 50);
+    expect(functions.lowAccount()).toEqual(25);
 })

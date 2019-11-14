c243cec32be7a31ba7b92994f4bddd18ecaaf9de
Brendan Stillwell <stillwellbrendan@gmail.com>
2019-11-14 13:06:30 -0700

    index functionality updated

--- src/03-Objects/scripts/comunityIndex.js
+++ src/03-Objects/scripts/comunityIndex.js
@@ -2,7 +2,7 @@ import {City, Community} from './community.js'
 import {cardAdder} from './comunityPresentation.js'
 let comm1 = new Community;
 createCity.addEventListener('click', (event) => {
-    comm1.createCity(nameText.value, latText.value, lonText.value, popText.value, comm1.Cities.key.counter);
+    comm1.createCity(nameText.value, Number(latText.value), Number(lonText.value), popText.value, comm1.Cities.key.counter);
     cities.appendChild(cardAdder(comm1.Cities.key.counter, comm1.whichSphere(comm1.Cities.key.counter), comm1.Cities[comm1.Cities.key.counter].latitude, comm1.Cities[comm1.Cities.key.counter].longitude, nameText.value, comm1.Cities[comm1.Cities.key.counter].population));
     comm1.Cities.key.increment();
     mostNorthern.innerText = comm1.getMostNorthern();
@@ -10,7 +10,14 @@ createCity.addEventListener('click', (event) => {
     population.innerText = comm1.getPopulation();
 })
 cities.addEventListener('click', (event) => {
-
+    let key = event.target.id.match(/[^a-z]/ig)[0];
+    let idRegex = new RegExp('[^'+key+']', 'g');
+    let id = event.target.id.match(idRegex);
+    id = id.reduce(function (sum, current){return sum + current}, '');
+    console.log(event.target.id);
+    console.log(idRegex);
+    console.log(id);
+    console.log(key);
 })
 export function cardValueAssigner(key){
 

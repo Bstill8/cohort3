137f733004165c49c9de73a75a80f95c63b2276b
Brendan Stillwell <stillwellbrendan@gmail.com>
2019-12-18 12:42:36 -0700

    search method can search for beginning end and by subject

--- react-02/src/components.js/linkedList.js
+++ react-02/src/components.js/linkedList.js
@@ -1,11 +1,31 @@
 
 export class LinkedList{
     constructor(){
-        this.header = {ref: null}
+        this.header = {content: {subject: null, ammount: null}, previous: null, next: null}
     }
-    search(location){
-
+    search(location, current = this.header){
+        if(location === 'end'){
+            if(current.next != null){
+                return this.search(location, current.next);
+            }
+            return current;
+        }
+        if(location === 'beginning'){
+            return this.header.next;
+        }
+        if(typeof location === 'string'){
+            if(location != current.content.subject){
+                if(current.next === null){
+                    return 'Subject does not exist';
+                }
+                return this.search(location, current.next)
+            }
+            return current;
+        }
+        
     }
     addNode(location, content){
+        let current = this.search(location);
+        current.next = {content: content, next: current.next, prev: current}
     }
 }
--- react-02/src/linkedList.test.js
+++ react-02/src/linkedList.test.js
@@ -1,8 +1,21 @@
 import {LinkedList} from './components.js/linkedList'
 
-test('test list adder', () => {
-
-})
+// test('test list adder', () => {
+//     let test = new LinkedList;
+//     test.addNode('end', 'hello');
+//     expect()
+// })
 test('test list searcher', ()=> {
+    let test = new LinkedList;
+    
+    expect(test.search('end')).toEqual({content: {subject: null, ammount: null}, previous: null, next: null})
+    test.header.next = {content: {subject: 'alpha', ammount: 45}, next: null, prev: test.header};
+    expect(test.search('end')).toEqual({content: {subject: 'alpha', ammount: 45}, next: null, prev: test.header})
+    test.header.next.next = {content: {subject: 'beta', ammount: 40}, next: null, prev: test.header.next}
+    expect(test.search('end')).toEqual({content: {subject: 'beta', ammount: 40}, next: null, prev: test.header.next})
+    expect(test.search('beginning')).toEqual({content: {subject: 'alpha', ammount: 45}, next: test.header.next.next, prev: test.header})
+    expect(test.search('alpha')).toEqual({content: {subject: 'alpha', ammount: 45}, next: test.header.next.next, prev: test.header})
+    expect(test.search('beta')).toEqual({content: {subject: 'beta', ammount: 40}, next: null, prev: test.header.next})
+    expect(test.search('charlie')).toEqual('Subject does not exist')
 
 })

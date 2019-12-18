656dbdad40612179a33cfa693ce864137f34690a
Brendan Stillwell <stillwellbrendan@gmail.com>
2019-12-18 14:31:55 -0700

    added delete method

--- react-02/src/components.js/linkedList.js
+++ react-02/src/components.js/linkedList.js
@@ -1,4 +1,4 @@
-
+//using a double linked list to make step backs easier
 export class LinkedList{
     constructor(){
         this.header = {content: {subject: null, ammount: null}, previous: null, next: null}
@@ -16,16 +16,33 @@ export class LinkedList{
         if(typeof location === 'string'){
             if(location != current.content.subject){
                 if(current.next === null){
-                    return 'Subject does not exist';
+                    return 'Subject \"' + location + '\" does not exist';
                 }
                 return this.search(location, current.next)
             }
             return current;
         }
+        if(typeof location === 'number'){
+            if(location === 0){
+                return current;
+            }
+            return this.search(location - 1, current.next);
+        }
         
     }
-    addNode(location, content){
+    addNode(location, content, ammount){
         let current = this.search(location);
-        current.next = {content: content, next: current.next, prev: current}
+        current.next = {content: {subject: content, ammount: ammount}, next: current.next, prev: current}
+    }
+    deleteNode(location){
+        let current = this.search(location);
+        if(typeof current === 'string'){
+            return current;
+        }
+        current.prev.next = current.next;
+        if(current.next != null){
+            current.next.prev = current.prev;
+        }
+        
     }
 }
--- react-02/src/linkedList.test.js
+++ react-02/src/linkedList.test.js
@@ -1,21 +1,44 @@
 import {LinkedList} from './components.js/linkedList'
 
-// test('test list adder', () => {
-//     let test = new LinkedList;
-//     test.addNode('end', 'hello');
-//     expect()
-// })
+
 test('test list searcher', ()=> {
     let test = new LinkedList;
     
-    expect(test.search('end')).toEqual({content: {subject: null, ammount: null}, previous: null, next: null})
+    expect(test.search('end')).toEqual(test.header)
     test.header.next = {content: {subject: 'alpha', ammount: 45}, next: null, prev: test.header};
-    expect(test.search('end')).toEqual({content: {subject: 'alpha', ammount: 45}, next: null, prev: test.header})
+    expect(test.search('end')).toEqual(test.header.next)
     test.header.next.next = {content: {subject: 'beta', ammount: 40}, next: null, prev: test.header.next}
-    expect(test.search('end')).toEqual({content: {subject: 'beta', ammount: 40}, next: null, prev: test.header.next})
-    expect(test.search('beginning')).toEqual({content: {subject: 'alpha', ammount: 45}, next: test.header.next.next, prev: test.header})
-    expect(test.search('alpha')).toEqual({content: {subject: 'alpha', ammount: 45}, next: test.header.next.next, prev: test.header})
-    expect(test.search('beta')).toEqual({content: {subject: 'beta', ammount: 40}, next: null, prev: test.header.next})
-    expect(test.search('charlie')).toEqual('Subject does not exist')
+    expect(test.search('end')).toEqual(test.header.next.next)
+    expect(test.search('beginning')).toEqual(test.header.next)
+    expect(test.search('alpha')).toEqual(test.header.next)
+    expect(test.search('beta')).toEqual(test.header.next.next)
+    expect(test.search('charlie')).toEqual('Subject "charlie" does not exist')
+    expect(test.search(1)).toEqual(test.header.next);
+    expect(test.search(2)).toEqual(test.header.next.next)
+    expect(test.search(0)).toEqual(test.header)
+
+})
+
+test('test list adder', () => {
+    let test = new LinkedList;
+    test.addNode('end', 'hello', 10);
+    expect(test.header.next).toEqual({content: {subject: 'hello', ammount: 10}, next: null, prev: test.header})
+    test.addNode('end', 'beach', 11);
+    expect(test.header.next).toEqual({content: {subject: 'hello', ammount: 10}, next: {content: {subject: 'beach', ammount: 11}, next: null, prev: test.header.next}, prev: test.header})
+})
+
+test('test delete', () => {
+    let test = new LinkedList;
+    test.addNode('end', 'alpha', 1)
+    test.addNode('end', 'beta', 2)
+    test.addNode('end', 'charlie', 3)
+    test.addNode('end', 'delta', 4)
+    expect(test.header.next.next.next.next).toEqual({content: {subject: 'delta', ammount: 4}, next: null, prev: test.header.next.next.next});
+    test.deleteNode('end');
+    expect(test.header.next.next.next).toEqual({content: {subject: 'charlie', ammount: 3}, next: null, prev: test.header.next.next});
+    test.deleteNode('beginning');
+    expect(test.header.next).toEqual({content: {subject: 'beta', ammount: 2}, next: {content: {subject: 'charlie', ammount: 3}, next: null, prev: test.header.next}, prev: test.header})
+    test.deleteNode(1);
+    expect(test.header.next).toEqual({content: {subject: 'charlie', ammount: 3}, next: null, prev: test.header})
 
 })

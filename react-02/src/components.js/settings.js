f033eeba2485239ad8c4b92f09988e23a7507a41
Brendan Stillwell <stillwellbrendan@gmail.com>
2020-01-23 11:30:59 -0700

    removed font size context

--- react-02/src/components.js/settings.js
+++ react-02/src/components.js/settings.js
@@ -9,9 +9,9 @@ function Settings() {
                         <input type="radio" name="color" value="red" checked={color === "#ff0000"} onClick={() => changeColor('red')} />Red
                         <input type="radio" name="color" value="blue" checked={color === "#4287f5"} onClick={() => changeColor('blue')} />Blue
                         <input type="radio" name="color" value="green" checked={color === "#45f542"} onClick={() => changeColor('green')} />Green <br />
-                        <input type="radio" name="font" value="small" checked={fontSize === '8px'} />Small
-                        <input type="radio" name="font" value="medium" checked={fontSize === '16px'} />Medium
-                        <input type="radio" name="font" value="large" checked={fontSize === '32px'} />Large
+                        {/* <input type="radio" name="font" value="small" checked={fontSize === "8px"} onClick={() => changeFont('small')}/>Small
+                        <input type="radio" name="font" value="medium" checked={fontSize === "16px"} onClick={() => changeFont('medium')}/>Medium
+                        <input type="radio" name="font" value="large" checked={fontSize === "32px"} onClick={() => changeFont('large')}/>Large */}
                     </div>
                     
                 )}

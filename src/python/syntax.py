ffa982cea1ca22a2ad0d86532b386d22e03d12bf
Brendan Stillwell <stillwellbrendan@gmail.com>
2020-01-27 15:58:46 -0700

    started syntax exercise working on imports and tests

---
+++ src/python/syntax.py
@@ -0,0 +1,13 @@
+number = 5
+string = "thing"
+boolean = True
+array = [5,3,6] #lists are used rather than arrays
+myObject = {"string": string, "boolean": boolean}
+if number == 5:
+    myObject.array = array
+else:
+    myObject.number = number
+
+def sum(a,b):
+    return 0 
+
--- src/python/syntax_test.py
+++ src/python/syntax_test.py
@@ -1,5 +1,10 @@
+import syntax
+
 def func(x):
     return x + 1
 
 def test_answer():
     assert func(3) == 4
+
+def test_sum():
+    assert syntax.sum(1,3) == 4

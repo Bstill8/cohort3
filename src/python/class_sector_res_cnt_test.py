3115f68f918b4cfdcf2ff37c314de2c1e74d18e9
Brendan Stillwell <stillwellbrendan@gmail.com>
2020-02-04 11:20:48 -0700

    created and tested function which creates an object with the total populations of all classes and sectors

---
+++ src/python/class_sector_res_cnt_test.py
@@ -0,0 +1,10 @@
+import class_sector_res_cnt
+classes_sectors = class_sector_res_cnt.class_sector_pop()
+def test_attributes_in_classes_sectors():
+    assert "classes" in classes_sectors
+    assert "sectors" in classes_sectors
+    assert "Industrial" in classes_sectors["classes"]
+    assert "Residential" in classes_sectors["classes"]
+    assert "CENTRE" in classes_sectors["sectors"]
+    assert "WEST" in classes_sectors["sectors"]
+

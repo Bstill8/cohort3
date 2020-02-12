f2e713c6d9755320377a5fccd0c05bd059bf70ca
Brendan Stillwell <stillwellbrendan@gmail.com>
2020-02-12 13:25:57 -0700

    array randomizer working

--- theory/sorting/sorting.cpp
+++ theory/sorting/sorting.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
 #include <ctime>
+#include <stdlib.h>
 using namespace std;
 
+int array_randomizer(int data[], int size);
 int main(){
-    int small[100];
-    int medium[10000];
-    int large[1000000];
+    int *small = new int[100];
+    int *medium = new int[10000];
+    int *large = new int[1000000];
     clock_t start;
     start = clock();
     double duration;
+    array_randomizer(small, 100);
+    cout << sizeof(small)/sizeof(int);
+    for(int i = 0; i<100; i++){
+        cout << small[i] << endl;
+    }
+
     
 
 
     return 0;
 }
 
-int array_generator(int data[]){
-    for(int i = 0; i<sizeof(data); i++){
+int array_randomizer(int data[], int size){
+    for(int i = 0; i<size; i++){
         data[i] = rand();
     }
     return 0;

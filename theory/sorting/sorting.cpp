3d18ae30eb1edf7e2a4c2c6f977a0be48eb0edb9
Brendan Stillwell <stillwellbrendan@gmail.com>
2020-02-14 18:01:53 -0700

    bubble sort working somewhat efficiently

--- theory/sorting/sorting.cpp
+++ theory/sorting/sorting.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 void array_randomizer(int data[], int32_t size);
 void bubble_sort(int data[], int32_t size);
+bool validator(int array[], int32_t size);
 int main()
 {
     int *small = new int[100];
@@ -13,12 +14,17 @@ int main()
     // clock_t start;
     // start = clock();
     // double duration;
-    array_randomizer(small, 100);
-    bubble_sort(small, 100);
-    for (int i = 0; i < 100; i++)
+    array_randomizer(large, 1000000);
+    bubble_sort(large, 1000000);
+    if(validator(large, 1000000))
     {
-        cout << small[i] << endl;
+        cout << "Valid";
+    }else
+    {
+        cout << "Invalid";
     }
+    
+
 
     delete small;
     delete medium;
@@ -35,32 +41,47 @@ void array_randomizer(int data[], int32_t size)
     }
     return;
 }
+bool validator(int array[], int32_t size){
+    for(int32_t i = 0; i < size - 1; i++)
+    {
+        if(array[i] > array[i+1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
 void bubble_sort(int data[], int32_t size)
 {    
     bool swapped = true;
+    int32_t iter = 1;
     while (swapped == true)
     {
         swapped = false;
         int current = data[0];
-        for (int32_t i = 0; i < size - 1; i++)
+        for (int32_t i = 0; i < size - iter; i++)
         {
             if (current >= data[i + 1])
             {
-                //cout << current << ' '<< data[i + 1] << endl;
                 data[i] = data[i + 1];
                 if (i == size - 2)
                 {
                     data[i + 1] = current;
                 }
+                swapped = true;
             }
             else
             {
-                
                 data[i] = current;
                 current = data[i + 1];
-                swapped = true;
+                
+            }
+            if(i == size - 2)
+            {
+                current = data[0];
             }
         }
+        iter ++;
     }
     return;
 }

31ff32be788ac951e6cd3a45450c157dde522ec4
Brendan Stillwell <stillwellbrendan@gmail.com>
2020-02-21 08:55:02 -0700

    started setting up flask application

---
+++ src/python/flask_app/app.py
@@ -0,0 +1,4 @@
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
--- theory/sorting/sorting.cpp
+++ theory/sorting/sorting.cpp
@@ -3,27 +3,40 @@
 #include <stdlib.h>
 using namespace std;
 
+int counter = 0;
 void array_randomizer(int data[], int32_t size);
 void bubble_sort(int data[], int32_t size);
+void selection_sort(int data[], int32_t size);
+void insertion_sort(int data[], int32_t size);
+void quick_sort(int data[], int32_t hi, int32_t lo = 0);
 bool validator(int array[], int32_t size);
 int main()
 {
+    int *test = new int[4];
     int *small = new int[100];
     int *medium = new int[10000];
     int *large = new int[1000000];
     // clock_t start;
     // start = clock();
     // double duration;
-    array_randomizer(large, 1000000);
-    bubble_sort(large, 1000000);
-    if(validator(large, 1000000))
+    
+    test[6];
+    test[3];
+    test[8];
+    test[1];
+    //array_randomizer(test, 4);
+    clock_t start = clock();
+    quick_sort(test, 4);
+    cout << counter;
+    clock_t end = clock();
+    if(validator(test, 4))
     {
-        cout << "Valid";
+        cout << "Valid" << endl;
     }else
     {
-        cout << "Invalid";
+        cout << "Invalid" << endl;
     }
-    
+    cout << (((float)(end - start))/CLOCKS_PER_SEC)/3600 << " hours";
 
 
     delete small;
@@ -82,6 +95,73 @@ void bubble_sort(int data[], int32_t size)
             }
         }
         iter ++;
+    }
+    return;
+}
+void selection_sort(int data[], int32_t size)
+{
+    int temp;
+    for(int32_t i = 0; i < size; i++)
+    {
+        for(int32_t j = i; j < size; j++)
+        {
+            if(data[j] < data[i])
+            {
+                temp = data[i];
+                data[i] = data[j];
+                data[j] = temp;
+            }
+        }
+    }
+    return;
+}
+void insertion_sort(int data[], int32_t size)
+{
+    int temp;
+    for(int32_t i = 1; i < size; i++)
+    {
+        temp = data[i];
+        for(int32_t j = i - 1; j >= 0; j--)
+        {
+            if(temp < data[j])
+            {
+                data[j + 1] = data[j];
+            }else
+            {
+                data[j + 1] = temp;
+            }
+            
+        }
+    }
+    return;
+}
+
+void quick_sort(int data[], int32_t hi, int32_t lo)
+{
+    counter++;
+    if(lo < hi)
+    {
+        int temp;
+        int pivot = hi - 1;
+        int i = lo;
+        for(int32_t j = lo; j < hi; j++)
+        {
+            if(data[j] < data[pivot])
+            {
+                temp = data[i];
+                data[i] = data[j];
+                data[j] = temp;
+                i++;
+            }
+        }
+        temp = data[i];
+        data[i] = data[pivot];
+        data[hi - 1] = temp;
+        
+        quick_sort(data, i, 0);
+        
+        quick_sort(data, hi, i + 1);
+        
     }
     return;
 }
